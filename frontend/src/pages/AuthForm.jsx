32a07a885b7b3d729dc44c62c951c0aa0f73a772
Helal <helaluddin6831@gmail.com>
2025-11-09 00:52:12 +0530

    backened  code update

--- frontend/src/pages/AuthForm.jsx
+++ frontend/src/pages/AuthForm.jsx
@@ -50,7 +50,7 @@ function AuthForm({ type }) {
       toast.success(res.data.message);
       navigate("/");
     } catch (error) {
-      toast.error(error);
+      toast.error(error.response.data.message);
     }
   }
   return (
